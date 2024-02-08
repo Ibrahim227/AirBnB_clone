1ded896b362734a94ddedcc7821e3b5ec60b54c3
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 15:59:17 -0800

    add freview

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""import required modue"""
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """class that inherit from BaseModel"""
+    def __init__(self):
+        """instantiate the contructor"""
+        pass
