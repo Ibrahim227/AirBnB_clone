aaef8b1f580799dbc908a634ceaf9a6bb7321b26
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 14:11:07 -0800

    update base_model

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Import required module"""
+
+
+class BaseModel:
+    """Class for all other classes to inherit from"""
+    def __init__(self):
+        """method to instantiate an instance of BaseMOdel"""
+    
