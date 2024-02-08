2ed3b8cec195102841c4820d60a6db6dbe0e3712
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 14:11:40 -0800

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -5,5 +5,4 @@
 class BaseModel:
     """Class for all other classes to inherit from"""
     def __init__(self):
-        """method to instantiate an instance of BaseMOdel"""
-    
+        """method to instantiate an instance of BaseMOdel"""    
