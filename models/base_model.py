50279496b7f0ee082c073ee94369d03a5cd5be46
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 14:42:30 -0800

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
         """method to instantiate an instance of BaseMOdel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = isoformat.now()
 
     def save(self):
         """update datetime"""
