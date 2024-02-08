1d6638f6d3b935aa203832182c37e05be0ee53c2
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 14:32:03 -0800

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,12 @@ import uuid
 
 class BaseModel:
     """Class for all other classes to inherit from"""
-    def __init__(self):
+    def __init__(self, id, created_at, updated_at):
         """method to instantiate an instance of BaseMOdel"""
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """update datetime"""
+        return updated_at
