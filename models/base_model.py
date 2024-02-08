60c09ac9b5af45b7387baaa0806dd03a66bf49c3
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 14:18:27 -0800

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Import required module"""
+from datetime import datetime
+import uuid
 
 
 class BaseModel:
     """Class for all other classes to inherit from"""
     def __init__(self):
         """method to instantiate an instance of BaseMOdel"""
+        self.id = str(uuid.uuid4())
