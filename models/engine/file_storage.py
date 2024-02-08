7651d9d1f0c7313f7433fc1092009bba838ecd0e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 15:58:38 -0800

    add fileStprage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Import the required module"""
+
+
+class FileStorage:
+    """Class to serialize/deserialize JSON file"""
+    def __init__(self):
+        """instantiate the constructor"""
+        pass
