651d225fde5290098d5c47f2366ab59ed731fd9b
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 15:14:38 -0800

    add amenity

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""import the required module"""
+from base_model import BaseModel
+
+
+class City(BaseModel):
+    """class City that inherit from BaseModel"""
+    def __init__(self):
+        """instantiate a constructor"""
+        pass
