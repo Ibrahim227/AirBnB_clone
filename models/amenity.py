164ee19e15e8f9e0df3b21ec29c647cc4ccc4c4c
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 15:12:26 -0800

    add amenity

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""import required module"""
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """class that inherit from BaseModel"""
+    def __init__(self):
+        """method the instantiate a constructor"""
+        pass
