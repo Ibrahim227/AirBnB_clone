3dc0b5ecfd77d02d6683891f5c9e1d81d47c3a7e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 16:00:04 -0800

    add place

---
+++ models/place.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""import required module"""
+from base_model import BaseModel
+
+
+class Place(BaseModel):
+    """class  that inherit from baseModel"""
+    def __init__(self);
+        """instantiate the constructor"""
+        pass
