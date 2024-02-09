3291ef008bab7f795ef25cd3488dc5b7f346a6bc
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-08 16:00:49 -0800

    add seISOwq

---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""import erequired module"""
+from base_model import BaseModel
+
+
+class User(BaseModel):
+    """class that inherit from baseModel"""
+    def __init__(self):
+        """instantiate the constructor
