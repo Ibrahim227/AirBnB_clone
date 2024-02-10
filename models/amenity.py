07812d4d62bb3842d1d7fa74f3e150788d89301a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 14:48:59 -0800

    update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """import required module"""
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     """class that inherit from BaseModel"""
     name: str = ""
-
-    super().__init__(self)
