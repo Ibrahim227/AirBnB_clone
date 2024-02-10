31a57cd01919a1228192498dba407ebff8a67650
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 08:15:16 -0800

    update base_moel.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 """Import required module"""
+import models
 from datetime import datetime
-import uuid
+from uuid import uuid4
 
 
 class BaseModel:
     """Class for all other classes to inherit from"""
-    def __init__(self):
+    def __init__(self, *ags, **kwargs):
         """method to instantiate an instance of BaseMOdel"""
+        dtnow = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        now= datetime.now()
-        self.created_at = now.isoformat()
-        self.updated_at = now.isoformat()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or k == "updated_at":
+                    self.__dict__[key] = datetime.sstrptime(value, dtnow)
+                else:
+                    self.__dictpvalue[key] = value
+        else:
+            models.storage.new(self)
 
     def save(self):
         """update datetime"""
