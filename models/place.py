3ab03a3f273702151783c3bbf6b462e073963e56
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 14:30:25 -0800

    update place.py

--- models/place.py
+++ models/place.py
@@ -5,6 +5,15 @@ from base_model import BaseModel
 
 class Place(BaseModel):
     """class  that inherit from baseModel"""
-    def __init__(self);
-        """instantiate the constructor"""
-        pass
+    city_id: str = ""
+    user_id: str = ""
+    name: str = ""
+    description: str = ""
+    number_rooms: int = 0
+    number_bathrooms: int = 0
+    max_guest: int = 0
+    price_by_night: int = 0
+    latitude: float = 0.0
+    longitude: float = 0.0
+    amenety_ids: str = []
+    super().__init__(self)
