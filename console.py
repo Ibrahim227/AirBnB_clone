114e59e78df433096b919df2c325d86eb00c988f
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 15:13:53 -0800

    update console.py

--- console.py
+++ console.py
@@ -3,20 +3,37 @@
 import cmd
 import sys
 
+
 class HBNBCommand(cmd.Cmd):
     """Contains the entry point of the CLi"""
     prompt = '(hbnb)'
-
-    def do_help(self, arg):
-        """Handle help command"""
-        pass
 
     def do_quit(self, arg):
         """quit command to exit the program"""
         sys.exit()
 
     def do_EOF(self, arg):
-        """Handle end of file"""
+        """End of file"""
+        pass
+
+    def create(self, arg):
+        """Creates a new instance of BaseModel, saves it to JSON"""
+        pass
+
+    def show(self, arg):
+        """Prints the str representation of an instance"""
+        pass
+
+    def destroy(self, arg):
+        """Deletes an instance and save the change to JSON file"""
+        pass
+
+    def all(self, arg):
+        """Prints all str representations of all instances"""
+        pass
+
+    def update(self, arg):
+        """Updates and instance by add/update attr, save change to JSON file"""
         pass
 
 
