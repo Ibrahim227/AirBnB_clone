ff4a3dad2aba9aa3c16ad199167a4965072ef8ee
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 14:49:43 -0800

    update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """import the required module"""
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
     """class City that inherit from BaseModel"""
     state_id: str = ""
     name: str = ""
-
-    super().__init__(self)
