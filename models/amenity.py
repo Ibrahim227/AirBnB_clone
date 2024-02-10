43d99e570bbbd68c2fe62bb423f90ac2055588de
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 14:26:48 -0800

    update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,6 @@ from base_model import BaseModel
 
 class Amenity(BaseModel):
     """class that inherit from BaseModel"""
-    def __init__(self):
-        """method the instantiate a constructor"""
-        pass
+    name: str = ""
+
+    super().__init__(self)
