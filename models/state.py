9ca9efa710aaae76a7a0853f9c5757443682946f
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-02-10 14:50:28 -0800

    update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """"import the required module"""
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
     """class that inherit fromBaseMOdel"""
     name: str = ""
-    super().__init__(self)
